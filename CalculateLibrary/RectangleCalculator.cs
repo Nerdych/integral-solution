5971e89dc2015d6de0e544cb65d145b4438373cb
Nikita Prodan <sa_vvk@mail.ru>
2021-03-27 19:05:31 +0300

    Добавьте файлы проекта.

---
+++ CalculateLibrary/RectangleCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateLibrary
+{
+    public class RectangleCalculator : ICalculator
+    {
+        public double Calculate(double a, double b, long n, Func<double, double> f)
+        {
+            double h = (b - a) / n;
+            a += h * 0.5;
+
+            double sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += f(a + h * i);
+            }
+
+            return sum * h;
+        }
+    }
+}
---
+++ IntegralSolution/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CalculateLibrary;
+using OxyPlot;
+
+namespace IntegralSolution
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            List<(double, int)> time = Calculate();
+            DrawGraph(time);
+        }
+
+        private void DrawGraph(List<(double, int)> times)
+        {
+            ClearGraph();
+            MainViewModel model = DataContext as MainViewModel;
+            model.Points.Clear();
+
+            foreach (var (time, count) in times)
+            {
+                model.Points.Add(new DataPoint(count, time));
+            }
+
+            graph.InvalidatePlot(true);
+            this.Cursor = Cursors.Arrow;
+        }
+
+        private void ClearGraph()
+        {
+            var model = DataContext as MainViewModel;
+            model.Points.Clear();
+            graph.InvalidatePlot(true);
+        }
+
+        private List<(double, int)> Calculate()
+        {
+            this.Cursor = Cursors.Wait;
+
+            double a = Convert.ToDouble(tbLowerBound.Text);
+            double b = Convert.ToDouble(tbUpperBound.Text);
+            long n = Convert.ToInt64(tbN.Text);
+
+            ICalculator calculator = GetCalculator();
+            List<(double, int)> time = new List<(double, int)>();
+
+            double result = 0.0;
+            for (int i = 0; i < n; i += 1000)
+            {
+                DateTime timeStart = DateTime.Now;
+
+                result = calculator.Calculate(a, b, i, x => 11 * x - Math.Log(11 * x) - 2);
+
+                DateTime timeStop = DateTime.Now;
+                time.Add(((timeStop - timeStart).TotalMilliseconds, i));
+            }
+
+            tbResult.Text = Convert.ToString(result);
+
+            return time;
+        }
+
+        private ICalculator GetCalculator()
+        {
+            switch (cbmMethod.SelectedIndex)
+            {
+                case 0:
+                    return new RectangleCalculator();
+                case 1:
+                    return new TrapCalculator();
+                default:
+                    return new RectangleCalculator();
+            }
+        }
+
+        private void btClear_Click(object sender, RoutedEventArgs e)
+        {
+            ClearGraph();
+        }
+    }
+}
